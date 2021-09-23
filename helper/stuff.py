3b84bfa0ef8a308915aee1060009fa278326614f
bereketx <91200890+Bereketx@users.noreply.github.com>
2021-09-23 10:16:08 -0700

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -40,18 +40,6 @@ async def start(event):
     )
 
 
-async def help(event):
-    await event.reply(
-        "**🐠 A Quality CompressorBot**\n\n+This Bot Compress Videos With Negligible Quality Change.\n+Generate Sample Compressed Video\n+Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress.\nSo Be patience Nd Send videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Get Options"
-    )
-
-
-async def ihelp(event):
-    await event.edit(
-        "**🐠 A Quality CompressorBot**\n\n+This Bot Compress Videos With Negligible Quality Change.\n+Generate Sample Compressed Video\n+Screenshots Too\n+Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress.\nSo Be patience Nd Send videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Get Options",
-        buttons=[Button.inline("BACK", data="beck")],
-    )
-
 
 
 async def sencc(e):
